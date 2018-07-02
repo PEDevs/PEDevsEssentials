70939273de9d79853a118598499e12d70683c7c5
Emirhan Akpınar <leetpocketmine@gmail.com>
2018-07-02 23:56:50 +0300

    Added repair all feature

--- src/PEDevs/Commands/Repair.php
+++ src/PEDevs/Commands/Repair.php
@@ -16,15 +16,41 @@ class Repair extends Command{
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if(!$this->testPermission($sender)) return false;
-
         if($sender instanceof Player){
-            $item = $sender->getInventory()->getItemInHand();
-            if($item->getId() !== 0){
-                $item->setDamage(0);
-                $sender->getInventory()->setItemInHand($item);
-                $sender->sendMessage(TextFormat::GREEN . "Repaired!");
+            if(empty($args[0]) {
+                $item = $sender->getInventory()->getItemInHand();
+                if($item->getId() == 0) {
+                    $sender->sendMessage(TextFormat::RED . "You can't repair your hand.");
+                    
+                }else{
+                    $item->setDamage(0);
+                    $sender->getInventory()->setItemInHand($item);
+                    $sender->sendMessage(TextFormat::GREEN . "You successfully repaired your item.");
+                    
+                }
+                
             }else{
-                $sender->sendMessage(TextFormat::RED . "You can not repair the air!");
+                switch($args[0]) {
+                       case "all":
+                        foreach($sender->getInventory()->getContents() as $items) {
+                            if($items->getId() == 0) {
+                               $sender->sendMessage(TextFormat::RED . "You don't have any repairable item.");
+                               
+                            }else{
+                                $items->setDamage(0);
+                                $sender->getInventory()->sendContents();
+                                $sender->sendMessage(TextFormat::GREEN . "You successfully repaired your all items.");
+                                
+                            ]
+                            
+                        }
+                        break;
+                       default:
+                        $sender->sendMessage(TextFormat::RED . "Missing args.");
+                        break;
+                       
+                }
+                
             }
         }else{
             $sender->sendMessage(TextFormat::RED . "Please use this command in game.");
