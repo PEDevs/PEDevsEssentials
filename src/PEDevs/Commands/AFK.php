76db3988001b73c9ec8eb788d7ac7caf3114496f
Eren Ahmed Akyol <ahmederen123@gmail.com>
2018-08-20 13:49:52 +0300

    Yokluktan çıkarıldı

--- src/PEDevs/Commands/AFK.php
+++ src/PEDevs/Commands/AFK.php
@@ -5,36 +5,36 @@
 * |  _ \ | ____||  _ \   ___ __   __ ___ 
 * | |_) ||  _|  | | | | / _ \\ \ / // __|
 * |  __/ | |___ | |_| ||  __/ \ V / \__ \
-* |_|    |_____||____/  \___|  \_/  |___/
-                                        
+* |_|    |_____||____/  \___|  \_/  |___/                                        
 */
 
 namespace PEDevs\Commands;
 
 use PEDevs\BaseAPI;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\utils\TextFormat;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\{Player, Server};
+use pocketmine\utils\TextFormat as TF;
 
 class AFK extends Command{
 	
-	/**@var BaseAPI*/
+	/** @var BaseAPI $plugin */
 	private $plugin;
 	
 	public function __construct(){
 		$this->plugin = BaseAPI::getInstance();
 		
-		parent::__construct("afk", "Afk Command");
+		parent::__construct("afk", "Afk komutu");
 	}
 	
 	public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-		
-		$sender->sendMessage(TextFormat::AQUA . "You are Now ".TextFormat::GREEN."AFK");
-
-		$this->getServer()->broadcastMessage("§c$sender->getName() §bis §aNow §bAFK");
+		if($this->plugin->getAfkState($sender) === null || $this->plugin->getAfkState($sender) == "no-afk"){         
+           $sender->sendMessage(TF::RED . "AFK Durumun " . TF::AQUA . "\"AFK\" " . TF::RED . "olarak ayarlandı");
+           $sender->setNameTag(TF::RED . "AFK\n" . $sender->getNameTag());
+           $this->plugin->setAfkState($sender, "afk");
+     }else{
+          $sender->sendMessage(TF::RED . "AFK Durumun " . TF::AQUA . "\"AFK Değil\" " . TF::RED . "olarak ayarlandı");
+         $sender->setNameTag($sender->getDisplayName());
+        $this->plugin->setAfkState($sender);
+     }
 	}
 }
-
-?>
